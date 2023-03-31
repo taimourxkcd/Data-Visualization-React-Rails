05cab045e0ee35afa322b991ae77030c0105c7e7
taimourz <taimour.rkt@gmail.com>
2023-04-01 02:36:46 +0500

    can access json data now at routes v1/csv

--- app/controllers/api/v1/csv_controller.rb
+++ app/controllers/api/v1/csv_controller.rb
@@ -1,10 +1,18 @@
 require "csv"
 
-class CsvController < ApplicationController
-  def index
-    @data = []
-    CSV.foreach("#{Rails.root}/app/data/sphere-sample-data.csv", headers: true) do |row|
-      @data << row.to_hash
+# app/controllers/api/v1/csv_api_controller.rb
+
+module Api
+  module V1
+    class CsvController < ApplicationController
+      # controller code here
+      def index
+        data = []
+        CSV.foreach("#{Rails.root}/app/data/sphere-sample-data.csv", headers: true) do |row|
+          data << row.to_hash
+        end
+        render json: data
+      end
     end
   end
 end

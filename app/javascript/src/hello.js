0fb92b943de4a0ea515a6d49cce518fbe0b3cf31
taimourz <taimour.rkt@gmail.com>
2023-04-01 04:05:03 +0500

    getting a single date

---
+++ app/javascript/src/hello.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Csv from "./src/Csv";
+
+export default class App extends Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <>
+        <h1>this is taimour</h1>
+        <Csv />
+      </>
+    );
+  }
+}

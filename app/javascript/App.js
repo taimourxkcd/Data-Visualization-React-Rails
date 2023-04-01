399aa79bf6776211409d68e5b527a2a9db56dd0e
taimourz <taimour.rkt@gmail.com>
2023-04-01 11:20:04 +0500

    getting data for a single column

--- app/javascript/App.js
+++ app/javascript/App.js
@@ -15,6 +15,7 @@ export default class App extends Component {
   }
 
   source = "/source/baseline";
+  optimised_target = "/optimisation_target/conversions";
 
   fetchData = async () => {
     let data = await fetch("/api/v1/csv/source/" + this.state.Id);
@@ -22,28 +23,32 @@ export default class App extends Component {
     this.setState({ Id: this.state.Id, Data: JsonData });
   };
 
-  handleSubmit = async(event)=>{
-    console.log(event.target.value)
-    await this.setState({Id: event.target.value, Data: this.state.Data})
-    this.fetchData()
-  }
-
+  handleSubmit = async (event) => {
+    console.log(event.target.value);
+    await this.setState({ Id: event.target.value, Data: this.state.Data });
+    this.fetchData();
+  };
 
   componentDidMount() {
     this.fetchData();
   }
 
-
-
   render() {
     const { Data } = this.state;
+
+    // console.log(this.state.Data);
+    // console.log(this.state.Data.optimisation_target);
+    // this.state.Data.forEach((item) => {
+    //   console.log(item.optimisation_target);
+    // });
 
     return (
       <>
         {/* <h1>this is taimour</h1>
         <Csv /> */}
 
-        <Header handle_submit = {this.handleSubmit}
+        <Header
+          handle_submit={this.handleSubmit}
           sourceName={
             this.state.Data && this.state.Data.length > 0
               ? this.state.Data[0].source
@@ -74,8 +79,34 @@ export default class App extends Component {
               />
             </div>
 
-        <ChartSection/>
-
+            <div className="row">
+              <div className="col-md-6">
+                {this.state.Data && this.state.Data.length > 0 ? (
+                  this.state.Data.map((item, index) => (
+                    <ChartSection
+                      key={index}
+                      attrib_conversions={item.attributed_revenue}
+                    />
+                  ))
+                ) : (
+                  <p>No data found.</p>
+                )}
+              </div>
+              <div className="col-md-6">
+                {this.state.Data && this.state.Data.length > 0 ? (
+                  this.state.Data.filter(
+                    (item) => item.optimisation_target === "conversions"
+                  ).map((item, index) => (
+                    <ChartSection
+                      key={index}
+                      conversions={item.attributed_conversions}
+                    />
+                  ))
+                ) : (
+                  <p>No data found.</p>
+                )}
+              </div>
+            </div>  
           </div>
         </div>
       </>

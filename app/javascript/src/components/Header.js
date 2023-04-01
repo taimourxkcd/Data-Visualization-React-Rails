1ae54811b055a1da5e4371ea9cb184d079f64a55
taimourz <taimour.rkt@gmail.com>
2023-04-01 07:35:15 +0500

    header is done

--- app/javascript/src/components/Header.js
+++ app/javascript/src/components/Header.js
@@ -4,7 +4,28 @@ export class Header extends Component {
   render(props) {
     return (
       <>
-        <h1>{this.props.sourceName}</h1>
+        <section className="header">
+          <div className="container-xxl d-flex justify-content-between align-items-center">
+            <div className="d-flex align-items-center">
+              <select
+                className="form-select form-select-lg me-3"
+                aria-label=".form-select-lg example"
+                name="select source"
+                onChange={this.props.handle_submit}
+              >
+                <option value="baseline">Baseline</option>
+                <option value="direct">direct</option>
+                <option value="radio">radio</option>
+              </select>
+            </div>
+            <div>
+              <h1 className="text-end">Company Name</h1>
+            </div>
+          </div>
+        </section>
+        <div className="sourceName">
+          <h1>{this.props.sourceName}</h1>
+        </div>
       </>
     );
   }
